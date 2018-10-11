5284743d84f9f43cc4ca70a7d6701308bb8cc820
PauloVitorRocha <paulovitorrocha.unb@gmail.com>
2018-10-11 00:45:40 -0300

    Creating new branch, and starting cars CRUD
    Co-authored-by: Joao Gabriel Rossi <bielrossiborba@gmail.com>
    Co-authored-by: Paulo Vitor Rocha <paulovitorrocha.unb@gmail.com>

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,7 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    modelo = models.CharField(max_length=50, required=False,  allow_blank=True)
+    cor = models.CharField(max_length=50, required=False,  allow_blank=True)
+    placa = models.CharField(max_length=8)
+    id_token = models.IntegerField()
--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,12 @@
-# from django.shortcuts import render
+from .serializers import CarSerializer
+from rest_framework.decorators import api_view
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from .models import Car
+import requests
 
-# Create your views here.
+
+class CarViewSet(ModelViewSet):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+
