f4fa4a5e6b4cd0de2270a479a38c9fa141df4c32
PauloVitorRocha <paulovitorrocha.unb@gmail.com>
2018-10-11 00:52:34 -0300

    Adapting the code to flake8 rules
    Co-authored-by: Joao Gabriel Rossi <bielrossiborba@gmail.com>
    Co-authored-by: Paulo Vitor Rocha <paulovitorrocha.unb@gmail.com>

--- cars/views.py
+++ cars/views.py
@@ -1,12 +1,8 @@
 from .serializers import CarSerializer
-from rest_framework.decorators import api_view
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.response import Response
 from .models import Car
-import requests
 
 
 class CarViewSet(ModelViewSet):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
-
