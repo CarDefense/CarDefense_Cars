8b75cbf6ca235161c1682b6256087c001aaeaebc
bielrossi15 <bielrossiborba@gmail.com>
2018-10-16 17:15:52 -0300

    Adding views.py HTTP responses tests
    Co-authored-by: Joao Gabriel Rossi <bielrossiborba@gmail.com>
    Co-authored-by: Paulo Vitor Rocha <paulovitorrocha.unb@gmail.com>

--- cars/tests.py
+++ cars/tests.py
@@ -1,3 +1,16 @@
-# from django.test import TestCase
+from rest_framework import status
+from .models import Car
+from rest_framework.test import APITestCase, APIClient, APIRequestFactory
+import json
 
-# Create your tests here.
+
+class NotificationsTests(APITestCase):
+
+    def testing_get(client):
+        client = APIClient()
+        client.get('http://192.168.0.9:8003/get_id_token/')
+
+    def testing_private(client):
+        client = APIClient()
+        client.post('/validate_car/', {'model': 'new', 'color': 'new2', 'plate': 'new3', 'id_token': '1'},
+                    format='json')
