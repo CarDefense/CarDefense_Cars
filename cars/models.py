c6844416653347e4b4d85d1dc07405c3bc60119f
João Gabriel Rossi <bielrossiborba@gmail.com>
2018-11-02 13:25:29 -0300

    Changing id_token IntegerField to BigIntegerField

--- cars/models.py
+++ cars/models.py
@@ -5,4 +5,4 @@ class Car(models.Model):
     model = models.CharField(max_length=50, blank=True, default='')
     color = models.CharField(max_length=50, blank=True, default='')
     plate = models.CharField(max_length=8)
-    id_token = models.IntegerField()
+    id_token = models.BigIntegerField()
